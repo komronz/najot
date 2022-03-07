90a44665751e6d67ae27eecd5f9fd5f1053998f9
Abdumalik <abdumaliksapoqulov@gmail.com>
2022-03-07 17:49:38 +0500

    .

--- lib/data/db/dao/task_model_dao.dart
+++ lib/data/db/dao/task_model_dao.dart
@@ -5,18 +5,18 @@ import 'package:floor/floor.dart';
 
 @dao
 abstract class TaskModelDao {
-  @insert
-  Future<int> insertTaskModel(TaskModel taskModel);
-
-  @delete
-  Future<int> deleteTaskModel(TaskModel taskModel);
-
-  @Query("DELETE FROM TaskModel")
-  Future<void> clearTaskTable();
-
-  @Query("SELECT * FROM TaskModel")
-  Future<List<TaskModel>> findAllTaskModels();
-
-  @Query("UPDATE TaskModel SET status = :status WHERE name = :name and dueDate = :dueDate")
-  Future<void> updateTaskModel(String name,int dueDate,int status);
+  // @insert
+  // Future<int> insertTaskModel(TaskModel taskModel);
+  //
+  // @delete
+  // Future<int> deleteTaskModel(TaskModel taskModel);
+  //
+  // @Query("DELETE FROM TaskModel")
+  // Future<void> clearTaskTable();
+  //
+  // @Query("SELECT * FROM TaskModel")
+  // Future<List<TaskModel>> findAllTaskModels();
+  //
+  // @Query("UPDATE TaskModel SET status = :status WHERE name = :name and dueDate = :dueDate")
+  // Future<void> updateTaskModel(String name,int dueDate,int status);
 }
