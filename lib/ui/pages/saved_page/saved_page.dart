4542685b8dc1de2a9f647146daafa91e289e21da
T460 <javohir@thinkland.uz>
2022-06-16 16:22:47 +0500

    change

--- lib/ui/pages/saved_page/saved_page.dart
+++ lib/ui/pages/saved_page/saved_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:najot/data/bloc/favorite-add_cubit/favorite_add_cubit.dart';
-import 'package:najot/data/bloc/saved_page_cubit/saved_page_cubit.dart';
 import 'package:najot/data/extensions/widget_padding_extension.dart';
 import 'package:najot/ui/pages/home_page/home_page.dart';
 import 'package:najot/ui/pages/saved_page/widgets/kraufanding_list_widget.dart';
@@ -18,52 +17,52 @@ class SavedPage extends StatelessWidget {
   const SavedPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
-    // return BlocProvider(
-    //     create: (context)=>FavoriteAddCubit()..getFavoriteAddList(),
-    //     child: BlocBuilder<FavoriteAddCubit,FavoriteAddState>(
-    //       builder: (context, state) => Scaffold(
-    //         appBar: AppBar(
-    //           automaticallyImplyLeading: false,
-    //           backgroundColor: Colors.transparent,
-    //           elevation: 0,
-    //           titleSpacing: 0,
-    //           title: Row(
-    //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //             children: [
-    //               InkWell(
-    //                 child: SvgPicture.asset(
-    //                   AppImageUtils.MENU,
-    //                   height: 35.w,
-    //                   width: 35.w,
-    //                 ),
-    //                 onTap: () {
-    //                   HomePage.globalKey.currentState!.openDrawer();
-    //                 },
-    //               ),
-    //               AppWidgets.textLocale(
-    //                 text: LocaleKeys.saved,
-    //                 fontSize: 26.sp,
-    //                 fontWeight: FontWeight.w600,
-    //               ),
-    //               InkWell(
-    //                 onTap: (){
-    //                   NavigatorService.to.pushNamed(NotificationPage.routeName,);
-    //                 },
-    //                 child: SvgPicture.asset(
-    //                   AppImageUtils.NOTIFICATION,
-    //                   height: 35.w,
-    //                   width: 35.w,
-    //                   fit: BoxFit.fill,
-    //                 ),
-    //               )
-    //             ],
-    //           ).paddingAll(20),
-    //         ),
-    //         body: _buildBody(context, state),
-    //       ),),
-    //
-    // );
+
+    return BlocProvider(
+        create: (context)=>FavoriteAddCubit()..getFavoriteAddList(),
+        child: BlocBuilder<FavoriteAddCubit,FavoriteAddState>(
+          builder: (context, state) => Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              titleSpacing: 0,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    child: SvgPicture.asset(
+                      AppImageUtils.MENU,
+                      height: 35.w,
+                      width: 35.w,
+                    ),
+                    onTap: () {
+                      HomePage.globalKey.currentState!.openDrawer();
+                    },
+                  ),
+                  AppWidgets.textLocale(
+                    text: LocaleKeys.saved,
+                    fontSize: 26.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  InkWell(
+                    onTap: (){
+                      NavigatorService.to.pushNamed(NotificationPage.routeName,);
+                    },
+                    child: SvgPicture.asset(
+                      AppImageUtils.NOTIFICATION,
+                      height: 35.w,
+                      width: 35.w,
+                      fit: BoxFit.fill,
+                    ),
+                  )
+                ],
+              ).paddingAll(20),
+            ),
+            body: _buildBody(context, state),
+          ),),
+
+    );
   }
   Widget _buildBody(BuildContext context, FavoriteAddState state,
       ) {
