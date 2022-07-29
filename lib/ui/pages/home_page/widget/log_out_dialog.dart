eb06082981dbd16fc1dd9673c9c651cfa75d06b6
TORNADO <ozodbek@thinkland.uz>
2022-07-29 17:16:58 +0300

    ui bug fix

--- lib/ui/pages/home_page/widget/log_out_dialog.dart
+++ lib/ui/pages/home_page/widget/log_out_dialog.dart
@@ -1,10 +1,13 @@
 
 
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:najot/data/extensions/widget_padding_extension.dart';
 import 'package:najot/data/localization/locale_keys.g.dart';
+import 'package:najot/data/services/hive_service.dart';
 import 'package:najot/data/utils/app_color_utils.dart';
 import 'package:najot/ui/pages/main_page/widgets/button_card_widget.dart';
 import 'package:najot/ui/widgets/app_widgets.dart';
@@ -38,55 +41,45 @@ class LogOutDialog extends StatelessWidget {
               fontSize: 20.sp,
               color: AppColorUtils.DARK2,
               textAlign: TextAlign.center,
+              maxLines: 2,
               text:  "Dasturdan chiqmoqchimisiz?",
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AppWidgets.textLocale(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18.sp,
-                  color: AppColorUtils.DARK2,
-                  textAlign: TextAlign.center,
-                  text: LocaleKeys.summa,
-                ),
-                AppWidgets.textLocale(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18.sp,
-                    color: AppColorUtils.PERCENT_COLOR,
-                    textAlign: TextAlign.center,
-                    text: LocaleKeys.sum,
-                    args: ["1 000"]
-                ),
-              ],
-            ),
-            SizedBox(height: 10.w,),
+            ).paddingSymmetric(vertical: 50.w,horizontal: 20),
 
-            ButtonCard(
-              onPress: () {},
-              text: LocaleKeys.switch_amount.tr(),
-              textSize: 16.sp,
-              fontWeight: FontWeight.w500,
-              height: 50.w,
-              width: 296.w,
-              color: AppColorUtils.PERCENT_COLOR,
-              textColor: AppColorUtils.WHITE,
-              borderRadius: 10,
-            ),
-            SizedBox(height: 12.w,),
-            ButtonCard(
-              onPress: () {
-                Navigator.pop(context);
-              },
-              text: LocaleKeys.exit.tr(),
-              textSize: 16.sp,
-              fontWeight: FontWeight.w500,
-              height: 50.w,
-              width: 296.w,
-              color: AppColorUtils.GREEN_ACCENT5,
-              textColor: AppColorUtils.BLACK,
-              borderRadius: 10,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ButtonCard(
+                  onPress: () {
+                    Navigator.pop(context);
+                  },
+                  text: "Yo'q",
+                  textSize: 16.sp,
+                  fontWeight: FontWeight.w500,
+                  height: 50.w,
+                  width: 100.w,
+                  color: AppColorUtils.GREEN_ACCENT5,
+                  textColor: AppColorUtils.BLACK,
+                  borderRadius: 10,
+                ),
+                ButtonCard(
+                  onPress: () {
+                    HiveService.to.deleteToken();
+                    exit(0);
+                  },
+                  text: "Ha",
+                  textSize: 16.sp,
+                  fontWeight: FontWeight.w500,
+                  height: 50.w,
+                  width: 100.w,
+                  color: AppColorUtils.PERCENT_COLOR,
+                  textColor: AppColorUtils.WHITE,
+                  borderRadius: 10,
+                ),
+
+
+              ],
             )
+
           ],
         ),
       ),
